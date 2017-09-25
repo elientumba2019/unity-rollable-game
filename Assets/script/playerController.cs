bb432d495e83d2c44e256e6032cedfdae3eaa30a
elie <elientumba@live.fr>
2017-09-26 05:42:05 +0800

    adding the reigid body componement to the ball and the script behing its movement

---
+++ Assets/script/playerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class playerController : MonoBehaviour {
+
+
+    private Rigidbody rb;
+    public float speed;
+
+
+    void Start()
+    {
+
+        //getting the componement from the game object
+        rb = GetComponent<Rigidbody>();
+
+    }
+
+
+    void Update()
+    {
+
+    }
+
+
+
+    //physics method
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertial = Input.GetAxis("Vertical");
+
+
+        Vector3 movement = new Vector3(moveHorizontal, 0, moveVertial);
+
+        rb.AddForce(movement * speed);
+    }
+}
